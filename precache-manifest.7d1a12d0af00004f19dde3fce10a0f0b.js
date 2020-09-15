fa925b9445af6da20062508e40afa27277828b05
Sujan Vaidya <shujan1992@gmail.com>
2020-09-15 10:18:36 +0545

    Updates

---
+++ precache-manifest.7d1a12d0af00004f19dde3fce10a0f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecf13913d734f993a818dca916d9576",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "57fcff8d90f0d301e226",
+    "url": "/react-tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "185550ba4c511e915492",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fcff8d90f0d301e226",
+    "url": "/react-tic-tac-toe/static/js/main.79e95c45.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
